9c1b3fcadc9f0c811a2fd16372f528eefeb9f33a
jimoh-sodiq <jimohsodiq301@gmail.com>
2022-04-28 01:32:15 +0100

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <main>
-    <h1 class="text-xs">Hello world</h1>
-  </main>
+  <div class="flex flex-col md:flex-row min-h-screen">
+    <!-- first section of page here -->
+
+    <section class="bg-white w-full md:w-1/3"></section>
+
+    <!-- Second section of page here -->
+
+    <section class="bg-gray-300 w-full md:w-2/3"></section>
+  </div>
 </template>
